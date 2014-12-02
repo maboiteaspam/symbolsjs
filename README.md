dbbcab9fda41d6e6cbf84145b4a427096c702340
clement <maboiteaspam@wanadoo.fr>
2014-12-02 23:52:09 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# SymbolsJS
+
+A library to display symbols on the command line
+```
+var symbols = {
+  ok: '✓',
+  err: '✖',
+  dot: '․'
+};
+```
+
+# Install
+
+```
+npm i maboiteaspam/symbolsjs
+```
+
+# Notes
+
+Initially Extracted from mocha.
---
+++ index.js
@@ -0,0 +1,16 @@
+// extracted from
+// https://github.com/mochajs/mocha/blob/master/lib/reporters/base.js
+var symbols = {
+  ok: '✓',
+  err: '✖',
+  dot: '․'
+};
+
+// With node.js on Windows: use symbols available in terminal default fonts
+if ('win32' == process.platform) {
+  symbols.ok = '\u221A';
+  symbols.err = '\u00D7';
+  symbols.dot = '.';
+}
+
+exports = module.exports = symbols;
